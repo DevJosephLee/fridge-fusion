7f296473ffa4deae8cddbd887d6f2a0bc7a2caf0
Joseph Lee <teddylou0215@gmail.com>
2023-11-26 15:40:47 -0800

    update styling

--- app/_components/nav.jsx
+++ app/_components/nav.jsx
@@ -3,11 +3,11 @@ import { Col, Container, Navbar } from "react-bootstrap";
 
 export default function Nav(props) {
   return (
-    <Navbar>
+    <Navbar className="shadow">
       <Container>
         <Col>
-          <Link href="/">
-            <h2 className="fst-italic">FridgeFusion</h2>
+          <Link className="fs-2 fw-bold fst-italic" href="/">
+            FridgeFusion
           </Link>
         </Col>
         <Col className="d-flex justify-content-end">
