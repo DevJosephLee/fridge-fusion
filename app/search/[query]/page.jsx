9e6ee825d840ff77cb7e021721092c6bea5aaf3e
Joseph Lee <teddylou0215@gmail.com>
2023-12-20 21:01:05 -0800

    increase number of search results, adjust styling

--- app/search/[query]/page.jsx
+++ app/search/[query]/page.jsx
@@ -4,15 +4,15 @@ import DishCard from '@/app/_components/dish-card'
 
 export default async function SearchResult({ params }) {
   const searchQuery = decodeURIComponent(params.query);
-  const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&number=4&apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}`)
+  const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&number=20&apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}`)
   const data = await res.json();
 
   return (
     <Container className="my-4">
       <Row className="mb-3">
-        <h5>Search results for {searchQuery}</h5>
+        <h5>Search results for &quot;{searchQuery}&quot;</h5>
       </Row>
-      <Row>
+      <Row className="mb-3">
         {
           data.results.map(recipeData => {
             const recipeProps = {
@@ -20,9 +20,8 @@ export default async function SearchResult({ params }) {
               title: recipeData.title,
               src: recipeData.image
             }
-
             return (
-              <Col key={recipeData.id} className="flex-basis mb-4 mb-xl-0">
+              <Col key={recipeData.id} className="flex-basis mb-4">
                 <DishCard {...recipeProps} />
               </Col>
             )
