6b5a3e83e6c63fe4f8150cc2839b588403cc55a8
Joseph Lee <teddylou0215@gmail.com>
2024-01-01 15:56:10 -0800

    shift down hero image

--- app/page.jsx
+++ app/page.jsx
@@ -17,6 +17,7 @@ export default async function Home() {
               alt="Home"
               fill
               style={{ objectFit: "cover" }}
+              objectPosition='50% 60%'
               priority
               className="rounded-3"
               sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33.3vw"
