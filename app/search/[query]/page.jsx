a1c921da6ec636efe28f95669656ccd94bbd8eb2
Joseph Lee <teddylou0215@gmail.com>
2024-01-02 17:04:03 -0800

    cleanup code

--- app/search/[query]/page.jsx
+++ app/search/[query]/page.jsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import { Col, Container, Row } from 'react-bootstrap'
-import DishCard from '@/app/_components/dish-card'
+import Image from 'next/image';
+import { Col, Container, Row } from 'react-bootstrap';
+import DishCard from '@/app/_components/dish-card';
 
 export default async function SearchResult({ params }) {
   const searchQuery = decodeURIComponent(params.query);
-  const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&number=20&apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}`)
+  const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&number=20&apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}`);
   const data = await res.json();
   const searchMessage = data.results.length >= 1 ? "Search results for " : "We didn't find any results for ";
 
@@ -20,15 +20,16 @@ export default async function SearchResult({ params }) {
               id: recipeData.id,
               title: recipeData.title,
               src: recipeData.image
-            }
+            };
+
             return (
               <Col md={12} lg={6} xl={4} key={recipeData.id} className="flex-basis mb-4">
                 <DishCard {...recipeProps} />
               </Col>
-            )
+            );
           })
         }
       </Row>
     </Container>
-  )
-}
+  );
+};
